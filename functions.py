a09e3985c16b92dfc86c75c1c388c03fb0180e93
Xin Yao <90070559+XYao2021@users.noreply.github.com>
2022-08-31 17:54:39 -0600

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,128 @@
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import struct
+import numpy as np
+import pickle
+import copy
+import socket
+import argparse
+
+
+HEADERSIZE = 10  # Bytes
+FORMAT = 'utf-8'
+SIZE = 1024
+
+def args_parser():
+    parse = argparse.ArgumentParser()
+
+    parse.add_argument('-agg', type=int, default=10, help='Global Aggregation times')
+    parse.add_argument('-lr', type=float, default=0.1, help='Learning Rate of the Model')
+    parse.add_argument('-bs', type=int, default=16, help='Batch Size for model')
+    parse.add_argument('-ts', type=list, default=[0, 2, 4, 6, 8, 9], help='Target set for training and local testing')
+    parse.add_argument('-iter', type=int, default=20, help='Local Training Times: Iterations')
+    # parse.add_argument('-tsn', type=int, default=3, help='The number of target the target datasets have')
+
+    parse.add_argument('-server', type=str, default='172.16.0.1', help='Server IP address')
+    parse.add_argument('-port', type=int, default=5050, help='Socket port')
+    parse.add_argument('-bond', type=int, default=4, help='Threshold for FedAvg on Sever side')
+    parse.add_argument('-name', type=int, default=1, help='Client Name')
+
+    args = parse.parse_args()
+    return args
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) * 100
+    return acc
+
+class MNISTModel(nn.Module):  # Improve the model V0 with nonlinear activation function nn.Relu()
+    def __init__(self, input_shape,
+                 output_shape,
+                 hidden_units):
+        super().__init__()
+        self.layer_stack = nn.Sequential(nn.Flatten(),  # Equal to x.view(-1, 784)
+                                         nn.Linear(in_features=input_shape, out_features=hidden_units),
+                                         nn.ReLU(),
+                                         nn.Linear(in_features=hidden_units, out_features=output_shape))
+
+    def forward(self, x):
+        return self.layer_stack(x)
+
+def train_step(model,
+               data_loader,
+               loss_fn,
+               optimizer,
+               accuracy_fn,
+               device,
+               ITERATION):
+
+    train_loss, train_acc = 0, 0
+    model.train()
+    for batch, (X, y) in enumerate(data_loader):
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        train_acc += accuracy_fn(y, y_pred.argmax(dim=1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print('ITERATION: ', ITERATION, '|', 'Train Loss: ', train_loss.item())
+    return train_loss.item(), model.state_dict()
+
+def test_step(model,
+              data_loader,
+              loss_fn,
+              accuracy_fn,
+              device,
+              ITERATION):
+    test_loss, test_acc = 0, 0
+
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            X, y = X.to(device), y.to(device)
+            test_pred = model(X)
+
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y, test_pred.argmax(dim=1))
+        # Adjust metrics and print out
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+        print('ITERATION: ', ITERATION, '|', 'Test Loss: ', test_loss.item(), '|', 'Test Acc: ', round(test_acc, 4), '%\n')
+    return test_loss.item(), test_acc
+
+def FedAvg(w):
+    w_avg = copy.deepcopy(w[0])
+    for k in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[k] += w[i][k]
+        w_avg[k] = torch.div(w_avg[k], len(w))
+    return w_avg
+
+def send_msg(sock, msg):
+    msg_pickle = pickle.dumps(msg)
+    sock.sendall(struct.pack(">I", len(msg_pickle)))
+    sock.send(msg_pickle)
+    # print(msg[0], 'sent to', sock.getpeername(), '\n')
+
+def recv_msg(sock, expect_msg_type=None):
+    msg_len = struct.unpack(">I", sock.recv(4))[0]
+    msg = sock.recv(msg_len, socket.MSG_WAITALL)
+    msg = pickle.loads(msg)
+    # print(msg[0], 'received from', sock.getpeername(), '\n')
+
+    if (expect_msg_type is not None) and (msg[0] != expect_msg_type):
+        raise Exception("Expected " + expect_msg_type + " but received " + msg[0])
+    return msg
+
---
+++ main.py
@@ -0,0 +1,146 @@
+import time
+import random
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+from functions import *
+import socket
+import copy
+from matplotlib import patheffects
+
+
+args = args_parser()
+SERVER = args.server
+PORT = args.port
+
+ADDR = (SERVER, PORT)
+CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+CLIENT.connect(ADDR)
+
+BATCH_SIZE = args.bs
+LEARNING_RATE = args.lr
+AGGREGATION = args.agg
+ITERATION = args.iter * AGGREGATION
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+train_data = datasets.MNIST(root="data",
+                            train=True,
+                            download=True,
+                            transform=ToTensor(),
+                            target_transform=None)
+
+test_data = datasets.MNIST(root="data",
+                           train=False,
+                           download=True,
+                           transform=ToTensor(),
+                           target_transform=None)
+
+# org_list = list(np.arange(10))
+# targets = random.sample(org_list, args.tsn)
+# print('This is target set: ', targets)
+if args.ts != [0, 2, 4, 6, 8, 9]:
+    targets = []
+    for t in args.ts:
+        targets.append(int(t))
+else:
+    targets = args.ts
+
+train_sets = []
+test_sets = []
+for i in range(len(train_data.targets)):
+    if train_data.targets[i] in targets:
+        train_sets.append(train_data[i])
+for j in range(len(test_data.targets)):
+    if test_data.targets[j] in targets:
+        test_sets.append(test_data[j])
+
+# print(len(train_sets), len(test_sets))
+
+train_dataloader = DataLoader(dataset=train_sets,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True)
+
+test_dataloader = DataLoader(dataset=test_data,
+                             batch_size=BATCH_SIZE,
+                             shuffle=False)
+
+torch.manual_seed(42)
+model = MNISTModel(input_shape=784,
+                   output_shape=10,
+                   hidden_units=50)
+model_test = copy.deepcopy(model)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model.parameters(),
+                            lr=LEARNING_RATE)
+# torch.manual_seed(42)
+Train_Loss = []
+Test_Loss, Test_Acc = [], []
+
+for iter in range(1, ITERATION + 1):
+    train_loss, w = train_step(model=model,
+                               data_loader=train_dataloader,
+                               loss_fn=loss_fn,
+                               optimizer=optimizer,
+                               accuracy_fn=accuracy_fn,
+                               device=device,
+                               ITERATION=iter)
+
+    Train_Loss.append(train_loss)
+
+    send_msg(CLIENT, ['MSG_CLIENT_TO_SERVER', w])
+
+    new_weights = recv_msg(CLIENT, 'MSG_SERVER_TO_CLIENT')
+    model_test.load_state_dict(new_weights[1])
+
+    test_loss, test_acc = test_step(model=model_test,
+                                    data_loader=test_dataloader,
+                                    loss_fn=loss_fn,
+                                    accuracy_fn=accuracy_fn,
+                                    device=device,
+                                    ITERATION=iter)
+
+    Test_Loss.append(test_loss)
+    Test_Acc.append(test_acc)
+
+    if iter % 10 == 0:
+        print('[NEW WEIGHTS LOAD] on number ', iter, 'iteration', '\n')
+        model.load_state_dict(new_weights[1])
+
+print('Train Loss: ', Train_Loss, '\n')
+print('Test Loss: ', Test_Loss, '\n')
+print('Test Acc: ', Test_Acc, '\n')
+
+txt_list = [['Train_Loss: ', Train_Loss],
+            ['Test_Loss: ', Test_Loss],
+            ['Test_Acc: ', Test_Acc]]
+
+# f = open('data_{}.txt'.format(args.name), 'w')
+# for item in txt_list:
+#     f.write("%s\n" % item)
+
+figure, axis = plt.subplots(1, 3)
+# For Sine Function
+axis[0].plot(range(len(Train_Loss)), Train_Loss, color='red', path_effects=[patheffects.SimpleLineShadow(shadow_color="blue", linewidth=5), patheffects.Normal()])
+axis[0].set_xlabel("Aggregation")
+axis[0].set_ylabel("Train Loss")
+axis[0].set_title("Training Loss Function")
+
+axis[1].plot(range(len(Test_Acc)), Test_Acc, color='blue', path_effects=[patheffects.SimpleLineShadow(shadow_color="blue", linewidth=5), patheffects.Normal()])
+axis[1].set_xlabel("Aggregation")
+axis[1].set_ylabel("Test Accuracy")
+axis[1].set_title("Test Accuracy Function")
+
+axis[2].plot(range(len(Test_Loss)), Test_Loss, color='green', path_effects=[patheffects.SimpleLineShadow(shadow_color="blue", linewidth=5), patheffects.Normal()])
+axis[2].set_xlabel("Aggregation")
+axis[2].set_ylabel("Test Loss")
+axis[2].set_title("Test Loss Function")
+
+plt.show()
+
